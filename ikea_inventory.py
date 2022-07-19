032c86fb9c53de55e4e81b4ee8d1844f32c44848
Andoni Sanguesa <andoniesanguesa@gmail.com>
2022-07-19 11:15:07 -0400

    added options for Selenium

--- ikea_inventory.py
+++ ikea_inventory.py
@@ -1,14 +1,17 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import chrome_selenium_updater.selenium_updater as updater
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.binary_location = "C:/Program Files/Google/Chrome Beta/Application/chrome.exe"
 
 updater.update_chrome_drivers()
 
 # Songesand Bed Frame, Songesand Night Stand, Songesand Dresser, Janinge Bar Stool, Toolstorp Coffee Table, Finalla Sleeper Sofa, Brimnes TV Unit, ÄPPLARÖ Table, Lagkapten Tabletop, Alex Drawers, Olov Leg
 items_to_check = ["s59241069", "70367444", "50366799", "10281354", "00400277", "s89319033", "50337698", "s99325458", "60487017", "60473548", "30264301"]
-base_url = "www.ikea.com/us/en/p/-"
+base_url = "https://www.ikea.com/us/en/p/-"
 
-selenium_driver = webdriver.Chrome()
+selenium_driver = webdriver.Chrome(chrome_options=options)
 
-for item in items_to_check:
-    selenium_driver.get(base_url + item)
+selenium_driver.get(base_url + items_to_check[0])
